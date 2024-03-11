28f3f10c93b4f6eb00d7db9749e0b72683dc1835
RadioJulius <9730701+RadioJulius@users.noreply.github.com>
2024-03-10 23:20:33 -0400

    commit xrave flashloan fix

--- contracts/protocol/libraries/logic/FlashLoanLogic.sol
+++ contracts/protocol/libraries/logic/FlashLoanLogic.sol
@@ -33,8 +33,7 @@ library FlashLoanLogic {
     using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
     using UserConfiguration for DataTypes.UserConfigurationMap;
 
-
-      struct FlashLoanLocalVars {
+    struct FlashLoanLocalVars {
         IFlashLoanReceiver receiver;
         address oracle;
         uint256 i;
@@ -57,7 +56,7 @@ library FlashLoanLogic {
         uint256 flashLoanPremiumTotal;
         uint256[] amounts;
         uint256[] modes;
-        bytes params; 
+        bytes params;
     }
 
     event FlashLoan(
@@ -68,112 +67,114 @@ library FlashLoanLogic {
         uint256 premium
     );
 
-  /**
-   * @dev Allows smartcontracts to access the liquidity of the pool within one transaction,
-   * as long as the amount taken plus a fee is returned.
-   * IMPORTANT There are security concerns for developers of flashloan receiver contracts that must be kept into consideration.
-   * For further details please visit https://developers.aave.com
-   * @param flashLoanParams struct containing receiverAddress, onBehalfOf, assets, amounts
-   **/
-  function flashLoan(
-    FlashLoanParams memory flashLoanParams,
-    mapping(uint256 => DataTypes.ReserveReference) storage reservesList,
-    mapping(address => DataTypes.UserConfigurationMap) storage usersConfig,
-    mapping(address => DataTypes.UserRecentBorrowMap) storage usersRecentBorrow,
-    mapping(address => mapping(bool => DataTypes.ReserveData)) storage reserves
-  ) external {
-    FlashLoanLocalVars memory vars;
+    /**
+     * @dev Allows smartcontracts to access the liquidity of the pool within one transaction,
+     * as long as the amount taken plus a fee is returned.
+     * IMPORTANT There are security concerns for developers of flashloan receiver contracts that must be kept into consideration.
+     * For further details please visit https://developers.aave.com
+     * @param flashLoanParams struct containing receiverAddress, onBehalfOf, assets, amounts
+     **/
+    function flashLoan(
+        FlashLoanParams memory flashLoanParams,
+        mapping(uint256 => DataTypes.ReserveReference) storage reservesList,
+        mapping(address => DataTypes.UserConfigurationMap) storage usersConfig,
+        mapping(address => DataTypes.UserRecentBorrowMap) storage usersRecentBorrow,
+        mapping(address => mapping(bool => DataTypes.ReserveData)) storage reserves
+    ) external {
+        FlashLoanLocalVars memory vars;
 
-    ValidationLogic.validateFlashloan(flashLoanParams.assets, flashLoanParams.amounts); //@todo add types array to this funciton too
+        ValidationLogic.validateFlashloan(flashLoanParams.assets, flashLoanParams.amounts); //@todo add types array to this funciton too
 
-    address[] memory aTokenAddresses = new address[](flashLoanParams.assets.length);
-    uint256[] memory premiums = new uint256[](flashLoanParams.assets.length);
+        address[] memory aTokenAddresses = new address[](flashLoanParams.assets.length);
+        uint256[] memory premiums = new uint256[](flashLoanParams.assets.length);
 
-    vars.receiver = IFlashLoanReceiver(flashLoanParams.receiverAddress);
+        vars.receiver = IFlashLoanReceiver(flashLoanParams.receiverAddress);
 
-    (aTokenAddresses, premiums) = getATokenAdressesAndPremiums(
-      flashLoanParams.receiverAddress,
-      flashLoanParams.assets,
-      flashLoanParams.reserveTypes,
-      flashLoanParams.amounts,
-      flashLoanParams.flashLoanPremiumTotal,
-      reserves);
+        (aTokenAddresses, premiums) = getATokenAdressesAndPremiums(
+            flashLoanParams.receiverAddress,
+            flashLoanParams.assets,
+            flashLoanParams.reserveTypes,
+            flashLoanParams.amounts,
+            flashLoanParams.flashLoanPremiumTotal,
+            reserves);
 
-    require(
-      vars.receiver.executeOperation(flashLoanParams.assets, flashLoanParams.amounts, premiums, msg.sender, flashLoanParams.params),
-      Errors.LP_INVALID_FLASH_LOAN_EXECUTOR_RETURN
-    );
-
-    for (vars.i = 0; vars.i < flashLoanParams.assets.length; vars.i++) {
-      vars.currentAsset = flashLoanParams.assets[vars.i];
-      vars.currentType = flashLoanParams.reserveTypes[vars.i];
-      vars.currentAmount = flashLoanParams.amounts[vars.i];
-      vars.currentPremium = premiums[vars.i];
-      vars.currentATokenAddress = aTokenAddresses[vars.i];
-
-      if (DataTypes.InterestRateMode(flashLoanParams.modes[vars.i]) == DataTypes.InterestRateMode.NONE) {
-        reserves[vars.currentAsset][vars.currentType].updateState();
-        reserves[vars.currentAsset][vars.currentType].cumulateToLiquidityIndex(
-          IERC20(vars.currentATokenAddress).totalSupply(),
-          vars.currentPremium
+        require(
+            vars.receiver.executeOperation(flashLoanParams.assets, flashLoanParams.amounts, premiums, msg.sender, flashLoanParams.params),
+            Errors.LP_INVALID_FLASH_LOAN_EXECUTOR_RETURN
         );
-        reserves[vars.currentAsset][vars.currentType].updateInterestRates(
-          vars.currentAsset,
+
+        for (vars.i = 0; vars.i < flashLoanParams.assets.length; vars.i++) {
+            vars.currentAsset = flashLoanParams.assets[vars.i];
+            vars.currentType = flashLoanParams.reserveTypes[vars.i];
+            vars.currentAmount = flashLoanParams.amounts[vars.i];
+            vars.currentPremium = premiums[vars.i];
+            vars.currentATokenAddress = aTokenAddresses[vars.i];
+
+            if (DataTypes.InterestRateMode(flashLoanParams.modes[vars.i]) == DataTypes.InterestRateMode.NONE) {
+                reserves[vars.currentAsset][vars.currentType].updateState();
+                reserves[vars.currentAsset][vars.currentType].cumulateToLiquidityIndex(
+                    IERC20(vars.currentATokenAddress).totalSupply(),
+          vars.currentPremium
+                );
+                reserves[vars.currentAsset][vars.currentType].updateInterestRates(
+                    vars.currentAsset,
           vars.currentATokenAddress,
           vars.currentAmount.add(vars.currentPremium),
           0
-        );
+                );
 
-        IERC20(vars.currentAsset).safeTransferFrom(
-          flashLoanParams.receiverAddress,
-          vars.currentATokenAddress,
-          vars.currentAmount.add(vars.currentPremium)
-        );
-      } else {
-        // If the user chose to not return the funds, the system checks if there is enough collateral and
-        // eventually opens a debt position
-        BorrowLogic.executeBorrow(
-          BorrowLogic.ExecuteBorrowParams(
-            vars.currentAsset,
-            vars.currentType,
-            msg.sender,
-            flashLoanParams.onBehalfOf,
-            vars.currentAmount,
-            vars.currentATokenAddress,
-            false,
-            flashLoanParams.addressesProvider,
-            flashLoanParams.reservesCount
-          ),
-            reserves,
-            reservesList,
-            usersConfig,
-            usersRecentBorrow
-        );
-      }
-      emit FlashLoan(
-        flashLoanParams.receiverAddress,
+                IERC20(vars.currentAsset).safeTransferFrom(
+                    flashLoanParams.receiverAddress,
+                    vars.currentATokenAddress,
+                    vars.currentAmount.add(vars.currentPremium)
+                );
+            } else {
+                // If the user chose to not return the funds, the system checks if there is enough collateral and
+                // eventually opens a debt position
+                BorrowLogic.executeBorrow(
+                    BorrowLogic.ExecuteBorrowParams(
+                        vars.currentAsset,
+                        vars.currentType,
+                        msg.sender,
+                        flashLoanParams.onBehalfOf,
+                        vars.currentAmount,
+                        vars.currentATokenAddress,
+                        false,
+                        flashLoanParams.addressesProvider,
+                        flashLoanParams.reservesCount
+                    ),
+                    reserves,
+                    reservesList,
+                    usersConfig,
+                    usersRecentBorrow
+                );
+            }
+            emit FlashLoan(
+                flashLoanParams.receiverAddress,
         msg.sender,
         vars.currentAsset,
         vars.currentAmount,
         vars.currentPremium
-      );
-    }
+            );
+        }
     }
 
-  function getATokenAdressesAndPremiums(
-    address receiverAddress,
-    address[] memory assets,
-    bool[] memory reserveTypes,
-    uint256[] memory amounts,
-    uint256 _flashLoanPremiumTotal,
-    mapping(address => mapping(bool => DataTypes.ReserveData)) storage _reserves
-  ) internal returns (address[] memory aTokenAddresses, uint256[] memory premiums) {
-    for (uint256 i = 0; i < assets.length; i++) {
-      aTokenAddresses[i] = _reserves[assets[i]][reserveTypes[i]].aTokenAddress;
+    function getATokenAdressesAndPremiums(
+        address receiverAddress,
+        address[] memory assets,
+        bool[] memory reserveTypes,
+        uint256[] memory amounts,
+        uint256 _flashLoanPremiumTotal,
+        mapping(address => mapping(bool => DataTypes.ReserveData)) storage _reserves
+    ) internal returns (address[] memory aTokenAddresses, uint256[] memory premiums) {
+        aTokenAddresses = new address[](assets.length);
+        premiums = new uint256[](assets.length);
+        for (uint256 i = 0; i < assets.length; i++) {
+            aTokenAddresses[i] = _reserves[assets[i]][reserveTypes[i]].aTokenAddress;
 
-      premiums[i] = amounts[i].mul(_flashLoanPremiumTotal).div(10000);
+            premiums[i] = amounts[i].mul(_flashLoanPremiumTotal).div(10000);
 
-      IAToken(aTokenAddresses[i]).transferUnderlyingTo(receiverAddress, amounts[i]);
+            IAToken(aTokenAddresses[i]).transferUnderlyingTo(receiverAddress, amounts[i]);
+        }
     }
-  }
 }
