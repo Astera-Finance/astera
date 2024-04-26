b338e62674091a2b8ca3898e590c409d7058b4f6
RadioJulius <9730701+RadioJulius@users.noreply.github.com>
2024-04-26 02:03:44 -0700

    fix aToken Space OnBehalfOf

--- contracts/protocol/tokenization/ERC6909/ATokenERC6909.sol
+++ contracts/protocol/tokenization/ERC6909/ATokenERC6909.sol
@@ -331,10 +331,10 @@ contract ATokenERC6909 is IncentivizedERC6909(), VersionedInitializable {
       require(amountScaled != 0, Errors.CT_INVALID_MINT_AMOUNT);
       _mint(onBehalfOf, id, amountScaled);
     } else {
-      previousBalance = super.balanceOf(user, id);
+      previousBalance = super.balanceOf(onBehalfOf, id);
       uint256 amountScaled = amount.rayDiv(index);
       require(amountScaled != 0, Errors.CT_INVALID_MINT_AMOUNT);
-      _mint(user, id, amountScaled);
+      _mint(onBehalfOf, id, amountScaled);
     }
 
     return previousBalance == 0;
