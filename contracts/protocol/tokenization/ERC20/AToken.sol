6b4ddbdbfc8ee5ccdf31598e7bfbd82dbadb1450
RadioJulius <9730701+RadioJulius@users.noreply.github.com>
2024-10-20 17:33:51 -0500

    zigtur M5

--- contracts/protocol/tokenization/ERC20/AToken.sol
+++ contracts/protocol/tokenization/ERC20/AToken.sol
@@ -598,7 +598,11 @@ contract AToken is
     }
 
     function setVault(address _vault) external override onlyLendingPool {
-        require(address(vault) == address(0), "84");
+        if(address(vault) != address(0)){
+            if(IERC20(address(vault)).balanceOf(address(this)) == 0){
+                revert("84");
+            }
+        }
         require(IERC4626(_vault).asset() == _underlyingAsset, "83");
         vault = IERC4626(_vault);
         IERC20(_underlyingAsset).forceApprove(address(vault), type(uint256).max);
