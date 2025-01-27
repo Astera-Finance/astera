43546a29a931dddc7d71605b50cd6e1f3108c084
Beirao <thomasmarques911@gmail.com>
2025-01-27 09:04:41 +0100

    Mitigation Spearbit: L48

--- contracts/protocol/core/interestRateStrategies/minipool/MiniPoolPiReserveInterestRateStrategy.sol
+++ contracts/protocol/core/interestRateStrategies/minipool/MiniPoolPiReserveInterestRateStrategy.sol
@@ -206,14 +206,11 @@ contract MiniPoolPiReserveInterestRateStrategy is
             // `&& utilizationRate != 0` to avoid 0 division. It's safe since the minipool flow is
             // always owed to a user. Since the debt is repaid as soon as possible if
             // `utilizationRate != 0` then `currentFlow == 0` by the end of the transaction.
-            uint256 reserveFactorComplement = PercentageMath.PERCENTAGE_FACTOR - reserveFactor;
-            if (
-                currentLiquidityRate < minLiquidityRate && utilizationRate != 0
-                    && reserveFactorComplement != 0
-            ) {
+            if (currentLiquidityRate < minLiquidityRate && utilizationRate != 0) {
                 currentLiquidityRate = minLiquidityRate;
-                currentVariableBorrowRate =
-                    currentLiquidityRate.rayDiv(utilizationRate.percentMul(reserveFactorComplement));
+                currentVariableBorrowRate = currentLiquidityRate.rayDiv(
+                    utilizationRate.percentMul(PercentageMath.PERCENTAGE_FACTOR - reserveFactor)
+                );
             }
         }
     }
