aa3a0a30d63402b0775c5b82b86e053e643825c7
xRave110 <xRave110@protonmail.com>
2025-08-29 08:36:25 +0200

    Etherex volatile twap

---
+++ tests/foundry/TwapOracle.t.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "./Common.sol";
+import "contracts/protocol/libraries/helpers/Errors.sol";
+import {WadRayMath} from "contracts/protocol/libraries/math/WadRayMath.sol";
+import {EtherexVolatileTwap} from "contracts/protocol/core/twaps/EtherexVolatileTwap.sol";
+
+contract TwapOracle is Common {
+    using WadRayMath for uint256;
+
+    ERC20[] erc20Tokens;
+    DeployedContracts deployedContracts;
+    ConfigAddresses configAddresses;
+
+    function setUp() public {
+        opFork = vm.createSelectFork(RPC, FORK_BLOCK);
+        assertEq(vm.activeFork(), opFork);
+        deployedContracts = fixture_deployProtocol();
+        configAddresses = ConfigAddresses(
+            address(deployedContracts.asteraDataProvider),
+            address(deployedContracts.stableStrategy),
+            address(deployedContracts.volatileStrategy),
+            address(deployedContracts.treasury),
+            address(deployedContracts.rewarder),
+            address(deployedContracts.aTokensAndRatesHelper)
+        );
+        fixture_configureProtocol(
+            address(deployedContracts.lendingPool),
+            address(commonContracts.aToken),
+            configAddresses,
+            deployedContracts.lendingPoolConfigurator,
+            deployedContracts.lendingPoolAddressesProvider
+        );
+        commonContracts.mockedVaults =
+            fixture_deployReaperVaultMocks(tokens, address(deployedContracts.treasury));
+        erc20Tokens = fixture_getErc20Tokens(tokens);
+        fixture_transferTokensToTestContract(erc20Tokens, 100_000 ether, address(this));
+    }
+
+    function testSetFallbackOracle() public {
+        ERC20[] memory erc20tokens = fixture_getErc20Tokens(tokens);
+        int256[] memory prices = new int256[](4);
+        uint256[] memory timeouts = new uint256[](4);
+        // All chainlink price feeds have 8 decimals
+        prices[0] = int256(95 * 10 ** PRICE_FEED_DECIMALS - 1); // USDC
+        prices[1] = int256(63_000 * 10 ** PRICE_FEED_DECIMALS); // WBTC
+        prices[2] = int256(3300 * 10 ** PRICE_FEED_DECIMALS); // ETH
+        prices[3] = int256(95 * 10 ** PRICE_FEED_DECIMALS - 1); // DAI
+        (, commonContracts.aggregators, timeouts) = fixture_getTokenPriceFeeds(erc20tokens, prices);
+
+        // EtherexVolatileTwap etherexVolatileTwap =
+        //     new EtherexVolatileTwap(address(tokens[0]), address(tokens[1]), 30 minutes, 1e18, true);
+
+        Oracle oracle = new Oracle(
+            tokens,
+            commonContracts.aggregators,
+            timeouts,
+            address(0),
+            address(0),
+            BASE_CURRENCY_UNIT,
+            address(deployedContracts.lendingPoolAddressesProvider)
+        );
+
+        // commonContracts.oracle.setFallbackOracle(address(etherexVolatileTwap));
+        // assertEq(address(etherexVolatileTwap), commonContracts.oracle.getFallbackOracle());
+    }
+}
