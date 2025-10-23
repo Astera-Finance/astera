66a4645f6520369f0ae7682be5a6407a4c4bf831
xRave110 <xRave110@protonmail.com>
2025-10-23 08:40:15 +0200

    Additional tests

--- tests/foundry/TestRestLiquidityIndex.t.sol
+++ tests/foundry/TestRestLiquidityIndex.t.sol
@@ -14,6 +14,10 @@ import {LendingPoolV3} from "contracts/protocol/core/lendingpool/LendingPoolV3.s
 import {IAToken} from "contracts/interfaces/IAToken.sol";
 import {IVariableDebtToken} from "contracts/interfaces/IVariableDebtToken.sol";
 import {Errors} from "contracts/protocol/libraries/helpers/Errors.sol";
+import {IAERC6909} from "contracts/interfaces/IAERC6909.sol";
+import {IMiniPoolAddressesProvider} from "contracts/interfaces/IMiniPoolAddressesProvider.sol";
+import {IMiniPool} from "contracts/interfaces/IMiniPool.sol";
+import {IMiniPoolConfigurator} from "contracts/interfaces/IMiniPoolConfigurator.sol";
 
 contract TestResetLiquidityIndexTest is Test {
     address constant DATA_PROVIDER = 0xE4FeC590F1Cf71B36c0A782Aac2E4589aFdaD88e;
@@ -25,16 +29,20 @@ contract TestResetLiquidityIndexTest is Test {
     address constant LENDING_POOL_ADDRESSES_PROVIDER = 0x9a460e7BD6D5aFCEafbE795e05C48455738fB119;
     address constant LENDING_POOL_PROXY = 0x17d8a5305A37fe93E13a28f09c46db5bE24E1B9E;
 
+    address constant MINI_POOL_ADDRESS_PROVIDER = 0x9399aF805e673295610B17615C65b9d0cE1Ed306;
+
     address constant USDC = 0x176211869cA2b568f2A7D4EE941E073a821EE1ff;
     address constant WETH = 0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f;
     address constant USDT = 0xA219439258ca9da29E9Cc4cE5596924745e12B93;
     address constant WBTC = 0x3aAB2285ddcDdaD8edf438C1bAB47e1a9D05a9b4;
     address constant ASUSD = 0xa500000000e482752f032eA387390b6025a2377b;
     address constant MUSD = 0xacA92E438df0B2401fF60dA7E4337B687a2435DA;
+    address constant LINEA = 0x1789e0043623282D5DCc7F213d703C6D8BAfBB04;
+    address constant REX33 = 0xe4eEB461Ad1e4ef8b8EF71a33694CCD84Af051C4;
+
     address constant ASUSDC = 0xcb338D6b4547479F5D11A68572F89A4F3cCa7347;
     address constant ASWETH = 0x78469e135ac38437cD4DfBf096b83f100EcF3260;
     address constant ASUSDT = 0xD66aD16105B0805e18DdAb6bF7792c4704568827;
-
     address constant ASWBTC = 0x4Ee17d24fBd633c128Fb5068d450e16D0Ff45108;
     address constant ASASUSD = 0x2a81FD13C0e101FCb96cB6fD996258e2b20d91d1;
     address constant ASMUSD = 0xb38064EF885551ef996c885Eb8Ea80Da5cC1c9f2;
@@ -46,7 +54,14 @@ contract TestResetLiquidityIndexTest is Test {
     address constant VDASUSD = 0xa04C8b74C9B1319DB240157cFe14504844debDf2;
     address constant VDMUSD = 0x20d2312769D6d9eAADBb57a3eEA44592440cd6C9;
 
+    address constant wasWBTC = 0x7dfd2F6d984CA9A2d2ffAb7350E6948E4315047b;
+    address constant wasWETH = 0x9A4cA144F38963007cFAC645d77049a1Dd4b209A;
+    address constant wasUSDC = 0xAD7b51293DeB2B7dbCef4C5c3379AfaF63ef5944;
+    address constant wasUSDT = 0x1579072d23FB3f545016Ac67E072D37e1281624C;
+
     uint128 constant NEW_LQUIDITY_INDEX = 1001883043396551183923396494; //24320598
+
+    mapping(address => uint256[]) miniPoolToBalances;
 
     function setUp() public {
         // LINEA setup
@@ -95,6 +110,11 @@ contract TestResetLiquidityIndexTest is Test {
             address(newLendingPoolV3)
         );
         vm.stopPrank();
+
+        console2.log(
+            "Lending pool revision:", LendingPoolV3(LENDING_POOL_PROXY).LENDINGPOOL_REVISION()
+        );
+
         // Unpause pool
         vm.startPrank(EMERGENCY);
         ILendingPoolConfigurator(LENDING_POOL_CONFIGURATOR).setPoolPause(false);
@@ -132,6 +152,39 @@ contract TestResetLiquidityIndexTest is Test {
         );
 
         uint256 totalSupplyOfUsdt = IAToken(ASUSDT).totalSupply();
+
+        // vm.startPrank(EMERGENCY);
+        // IMiniPoolConfigurator(0x41296B58279a81E20aF1c05D32b4f132b72b1B01).setPoolPause(
+        //     false, IMiniPool(0x52280eA8979d52033E14df086F4dF555a258bEb4)
+        // );
+        // vm.stopPrank();
+
+        // console2.log(
+        //     "Pool paused: ", IMiniPool(0x52280eA8979d52033E14df086F4dF555a258bEb4).paused()
+        // );
+        // console2.log(
+        //     "Pool paused: ", IMiniPool(0x65559abECD1227Cc1779F500453Da1f9fcADd928).paused()
+        // );
+        // console2.log(
+        //     "Pool paused: ", IMiniPool(0x0baFB30B72925e6d53F4d0A089bE1CeFbB5e3401).paused()
+        // );
+
+        // console2.log("Core Pool paused: ", ILendingPool(LENDING_POOL_PROXY).paused());
+
+        // vm.startPrank(ADMIN);
+        // IERC20Detailed(0xa500000000e482752f032eA387390b6025a2377b).approve(
+        //     0x52280eA8979d52033E14df086F4dF555a258bEb4, 1e18
+        // );
+        // IMiniPool(0x52280eA8979d52033E14df086F4dF555a258bEb4).liquidationCall(
+        //     0x9A4cA144F38963007cFAC645d77049a1Dd4b209A,
+        //     true,
+        //     0xa500000000e482752f032eA387390b6025a2377b,
+        //     false,
+        //     0xcd69567080Dccad1Afe61aCc022c0A7164B29AB4,
+        //     1e18,
+        //     false
+        // );
+        // vm.stopPrank();
 
         deal(USDT, 0xF1D6ab29d12cF2bee25A195579F544BFcC3dD78f, 1e10);
 
@@ -289,5 +342,105 @@ contract TestResetLiquidityIndexTest is Test {
         //     IERC20Detailed(USDT).balanceOf(ASUSDT) + IVariableDebtToken(VDUSDT).totalSupply(),
         //     "Wrong numbers for USDT"
         // );
+    }
+
+    function testBalancesBeforeAndAfterHack() public {
+        string memory lineaRpc = vm.envString("LINEA_RPC_URL");
+        uint256 lineaFork = vm.createSelectFork(lineaRpc, 24220150);
+        assertEq(vm.activeFork(), lineaFork);
+
+        uint256 initialAsUsdcBalance = IERC20Detailed(USDC).balanceOf(ASUSDC);
+        uint256 initialAsWethBalance = IERC20Detailed(WETH).balanceOf(ASWETH);
+        uint256 initialAsWbtcBalance = IERC20Detailed(WBTC).balanceOf(ASWBTC);
+        uint256 initialAsUsdtBalance = IERC20Detailed(USDT).balanceOf(ASUSDT);
+        uint256 initialAsAsUsdBalance = IERC20Detailed(ASUSD).balanceOf(ASASUSD);
+        uint256 initialAsmUsdBalance = IERC20Detailed(MUSD).balanceOf(ASMUSD);
+
+        console2.log("--------------------BEFORE-------------------------");
+
+        console2.log("initialAsWbtcBalance: ", initialAsWbtcBalance);
+        console2.log("initialAsWethBalance: ", initialAsWethBalance);
+        console2.log("initialAsUsdcBalance: ", initialAsUsdcBalance);
+        console2.log("initialAsUsdtBalance: ", initialAsUsdtBalance);
+        console2.log("initialAsAsUsdBalance: ", initialAsAsUsdBalance);
+        console2.log("initialAsmUsdBalance: ", initialAsmUsdBalance);
+
+        for (uint256 i = 0; i < 4; i++) {
+            address erc6909 =
+                (IMiniPoolAddressesProvider(MINI_POOL_ADDRESS_PROVIDER).getMiniPoolToAERC6909(i));
+            IMiniPool miniPool =
+                IMiniPool(IMiniPoolAddressesProvider(MINI_POOL_ADDRESS_PROVIDER).getMiniPool(i));
+            (address[] memory reserves,) = miniPool.getReservesList();
+            miniPoolToBalances[erc6909] = new uint256[](reserves.length);
+            for (uint256 idx = 0; idx < reserves.length; idx++) {
+                miniPoolToBalances[erc6909][idx] = IERC20Detailed(reserves[idx]).balanceOf(erc6909);
+                console2.log(
+                    "Initial balance for %s in MiniPool %s: %s",
+                    IERC20Detailed(reserves[idx]).symbol(),
+                    i,
+                    IERC20Detailed(reserves[idx]).balanceOf(erc6909)
+                );
+            }
+        }
+
+        console2.log("--------------------AFTER-------------------------");
+        lineaFork = vm.createSelectFork(lineaRpc, 24322904);
+        assertEq(vm.activeFork(), lineaFork);
+        console2.log("finalAsWbtcBalance: ", IERC20Detailed(WBTC).balanceOf(ASWBTC));
+        console2.log("finalAsWethBalance: ", IERC20Detailed(WETH).balanceOf(ASWETH));
+        console2.log("finalAsUsdcBalance: ", IERC20Detailed(USDC).balanceOf(ASUSDC));
+        console2.log("finalAsUsdtBalance: ", IERC20Detailed(USDT).balanceOf(ASUSDT));
+        console2.log("finalAsAsUsdBalance: ", IERC20Detailed(ASUSD).balanceOf(ASASUSD));
+        console2.log("finalAsmUsdBalance: ", IERC20Detailed(MUSD).balanceOf(ASMUSD));
+
+        console2.log(
+            "Diff finalAsWbtcBalance: ",
+            int256(initialAsWbtcBalance) - int256(IERC20Detailed(WBTC).balanceOf(ASWBTC))
+        );
+        console2.log(
+            "Diff finalAsWethBalance: ",
+            int256(initialAsWethBalance) - int256(IERC20Detailed(WETH).balanceOf(ASWETH))
+        );
+        console2.log(
+            "Diff finalAsUsdcBalance: ",
+            int256(initialAsUsdcBalance) - int256(IERC20Detailed(USDC).balanceOf(ASUSDC))
+        );
+        console2.log(
+            "Diff finalAsUsdtBalance: ",
+            int256(initialAsUsdtBalance) - int256(IERC20Detailed(USDT).balanceOf(ASUSDT))
+        );
+        console2.log(
+            "Diff finalAsAsUsdBalance: ",
+            int256(initialAsAsUsdBalance) - int256(IERC20Detailed(ASUSD).balanceOf(ASASUSD))
+        );
+        console2.log(
+            "Diff finalAsmUsdBalance: ",
+            int256(initialAsmUsdBalance) - int256(IERC20Detailed(MUSD).balanceOf(ASMUSD))
+        );
+
+        for (uint256 i = 0; i < 4; i++) {
+            address erc6909 =
+                (IMiniPoolAddressesProvider(MINI_POOL_ADDRESS_PROVIDER).getMiniPoolToAERC6909(i));
+            IMiniPool miniPool =
+                IMiniPool(IMiniPoolAddressesProvider(MINI_POOL_ADDRESS_PROVIDER).getMiniPool(i));
+            (address[] memory reserves,) = miniPool.getReservesList();
+            for (uint256 idx = 0; idx < reserves.length; idx++) {
+                console2.log(
+                    "Final balance for %s in MiniPool %s: %s",
+                    IERC20Detailed(reserves[idx]).symbol(),
+                    i,
+                    IERC20Detailed(reserves[idx]).balanceOf(erc6909)
+                );
+                // console2.log(
+                //     "Diff balance for %s in MiniPool %s: ",
+                //     IERC20Detailed(reserves[idx]).symbol(),
+                //     i
+                // );
+                // console2.log(
+                //     int256(miniPoolToBalances[erc6909][idx])
+                //         - int256(IERC20Detailed(reserves[idx]).balanceOf(erc6909))
+                // );
+            }
+        }
     }
 }
